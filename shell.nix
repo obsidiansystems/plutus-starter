ebf844113c8a156af459309542af127c37bf2269
Luigy Leon <luigy.leon@obsidian.systems>
2021-05-19 13:53:10 -0400

    Add tools as nativeBuildInputs
    plutus is doing the same. Not sure what may be causing cache misses when
    listing them under tools section.

--- shell.nix
+++ shell.nix
@@ -1,20 +1,22 @@
 let
   project = import ./default.nix;
 
-  inherit (project.plutus) pkgs;
+  inherit (project.plutus) plutus pkgs;
 
   inherit (pkgs.haskell-nix.haskellLib) selectProjectPackages;
 in
   project.haskellNixProject.shellFor {
     withHoogle = false;
+    nativeBuildInputs = with plutus; [
+      haskell-language-server
+      hlint
+      stylish-haskell
+    ];
     packages = ps: builtins.attrValues (selectProjectPackages ps);
     exactDeps = true;
 
     tools = {
       cabal = "latest";
-      # hlint = "latest";
-      # stylish-haskell = "latest";
-      # haskell-language-server = "latest";
     };
 
   }
