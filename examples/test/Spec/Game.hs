b32fb901a780222e2f811d03e4c34925c26ff0ac
Luigy Leon <luigy@outlook.com>
2021-06-15 13:11:17 -0400

    Sync up Game example
    and fix actually running the tests from plutus-stater

--- examples/test/Spec/Game.hs
+++ examples/test/Spec/Game.hs
@@ -1,22 +1,25 @@
 {-# LANGUAGE DataKinds        #-}
 {-# LANGUAGE TemplateHaskell  #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE NumericUnderscores     #-}
 
 module Spec.Game
     ( tests
     ) where
 
 import           Control.Monad         (void)
-import           Ledger.Ada            (adaValueOf, lovelaceValueOf)
-import           Plutus.Contract       (Contract, ContractError)
+import           Ledger                (ValidationError(ScriptFailure), ScriptError(EvaluationError))
+import qualified Ledger.Ada            as Ada
+import           Plutus.Contract       (Contract, ContractError(WalletError))
+import           Wallet.API (WalletAPIError(ValidationError))
 import           Plutus.Contract.Test
 import           Plutus.Contracts.Game
 import           Plutus.Trace.Emulator (ContractInstanceTag)
 import qualified Plutus.Trace.Emulator as Trace
 import qualified PlutusTx
-import qualified PlutusTx.Prelude      as PlutusTx
 import           Test.Tasty
 import qualified Test.Tasty.HUnit      as HUnit
+import Prelude hiding (not)
 
 w1, w2 :: Wallet
 w1 = Wallet 1
@@ -29,32 +32,47 @@ t2 = Trace.walletInstanceTag w2
 theContract :: Contract () GameSchema ContractError ()
 theContract = game
 
+-- W1 locks funds, W2 (and other wallets) should have access to guess endpoint
+-- No funds locked, so W2 (and other wallets) should not have access to guess endpoint
 tests :: TestTree
 tests = testGroup "game"
-    [ checkPredicate "Expose 'lock' and 'guess' endpoints"
-        (endpointAvailable @"lock" theContract (Trace.walletInstanceTag w1)
-        .&&. endpointAvailable @"guess" theContract  (Trace.walletInstanceTag w1))
-        $ void (Trace.activateContractWallet w1 theContract)
+    [ checkPredicate "Expose 'lock' endpoint, but not 'guess' endpoint"
+        (endpointAvailable @"lock" theContract t1
+          .&&. not (endpointAvailable @"guess" theContract t1))
+        $ void $ Trace.activateContractWallet w1 (lock @ContractError)
 
     , checkPredicate "'lock' endpoint submits a transaction"
-        (anyTx theContract (Trace.walletInstanceTag w1))
+        (anyTx theContract t1)
         $ do
             hdl <- Trace.activateContractWallet w1 theContract
-            Trace.callEndpoint @"lock" hdl (LockParams "secret" (adaValueOf 10))
+            Trace.callEndpoint @"lock" hdl (LockParams "secret" (Ada.adaValueOf 10))
 
     , checkPredicate "'guess' endpoint is available after locking funds"
-        (endpointAvailable @"guess" theContract (Trace.walletInstanceTag w2))
-        lockTrace
+        (endpointAvailable @"guess" theContract t2)
+        $ do
+          void $ Trace.activateContractWallet w2 theContract
+          lockTrace w1 "secret"
 
     , checkPredicate "guess right (unlock funds)"
-        (walletFundsChange w2 (lovelaceValueOf 10)
-            .&&. walletFundsChange w1 (lovelaceValueOf (-10)))
-        guessTrace
+        (walletFundsChange w2 (Ada.adaValueOf 10)
+          .&&. walletFundsChange w1 (Ada.adaValueOf (-10)))
+        $ do
+          lockTrace w1 "secret"
+          guessTrace w2 "secret"
 
     , checkPredicate "guess wrong"
-        (walletFundsChange w2 PlutusTx.zero
-            .&&. walletFundsChange w1 (lovelaceValueOf (-10)))
-        guessWrongTrace
+        (walletFundsChange w2 (Ada.lovelaceValueOf 0)
+          .&&. walletFundsChange w1 (Ada.adaValueOf (-10))
+          .&&. assertContractError guess t2 (== wrongGuessExpectedError) ("error should match with: " <> show wrongGuessExpectedError))
+        $ do
+          lockTrace w1 "secret"
+          guessTrace w2 "SECRET"
+
     , goldenPir "examples/test/Spec/game.pir" $$(PlutusTx.compile [|| validateGuess ||])
+
     , HUnit.testCase "script size is reasonable" (reasonable gameValidator 20000)
     ]
+
+wrongGuessExpectedError :: ContractError
+wrongGuessExpectedError =
+  WalletError (ValidationError (ScriptFailure (EvaluationError [])))
